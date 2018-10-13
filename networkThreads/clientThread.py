8cf242964e93e5fe352784cac0caf37e6a8190e6
sebnunez6 <snunez6@miners.utep.edu>
2018-10-13 15:48:34 +0000

    File sending implemented in between threads

--- networkThreads/clientThread.py
+++ networkThreads/clientThread.py
@@ -1,7 +1,7 @@
 #! /usr/bin/env python3
 
 # Echo client program
-import socket, sys, re
+import socket, sys, re, os
 import params
 from framedSock import FramedStreamSock
 from threading import Thread
@@ -14,20 +14,19 @@ else:
     port = "50001"
 
 switchesVarDefaults = (
-    (('-s', '--server'), 'server', "localhost:" +port),
-    (('-d', '--debug'), "debug", False), # boolean (set if present)
-    (('-?', '--usage'), "usage", False), # boolean (set if present)
-    )
+    (('-s', '--server'), 'server', "localhost:" + port),
+(('-d', '--debug'), "debug", False), # boolean (set if present)
+(('-?', '--usage'), "usage", False), # boolean (set if present)
+)
 
 
-progname = "clientThread"
+progname = "ClientThread"
 paramMap = params.parseParams(switchesVarDefaults)
 
 server, usage, debug  = paramMap["server"], paramMap["usage"], paramMap["debug"]
 
 if usage:
     params.usage()
-
 
 try:
     serverHost, serverPort = re.split(":", server)
@@ -42,40 +41,71 @@ class ClientThread(Thread):
         self.serverHost, self.serverPort, self.debug = serverHost, serverPort, debug
         self.start()
     def run(self):
-       s = None
-       for res in socket.getaddrinfo(serverHost, serverPort, socket.AF_UNSPEC, socket.SOCK_STREAM):
-           af, socktype, proto, canonname, sa = res
-           try:
-               print("creating sock: af=%d, type=%d, proto=%d" % (af, socktype, proto))
-               s = socket.socket(af, socktype, proto)
-           except socket.error as msg:
-               print(" error: %s" % msg)
-               s = None
-               continue
-           try:
-               print(" attempting to connect to %s" % repr(sa))
-               s.connect(sa)
-           except socket.error as msg:
-               print(" error: %s" % msg)
-               s.close()
-               s = None
-               continue
-           break
+        s = None
+        for res in socket.getaddrinfo(serverHost, serverPort, socket.AF_UNSPEC, socket.SOCK_STREAM):
+            af, socktype, proto, canonname, sa = res
+            try:
+                print("creating sock: af=%d, type=%d, proto=%d" % (af, socktype, proto))
+                s = socket.socket(af, socktype, proto)
+            except socket.error as msg:
+                print(" error: %s" % msg)
+                s = None
+                continue
+            try:
+                print(" attempting to connect to %s" % repr(sa))
+                s.connect(sa)
+            except socket.error as msg:
+                print(" error: %s" % msg)
+                s.close()
+                s = None
+                continue
+            break
 
-       if s is None:
-           print('could not open socket')
-           sys.exit(1)
+        if s is None:
+            print('could not open socket')
+            sys.exit(1)
 
-       fs = FramedStreamSock(s, debug=debug)
+        fs = FramedStreamSock(s, debug=debug)
+
+        files = os.listdir(os.curdir)  #grab current files
+        print(files)
+        inputFile = input("Please select a file to send \n")
+        #Attemps to read input as file
+        try:
+            with open(inputFile.strip(),"rb") as binary_file:
+                # Read the whole file at once
+                data = binary_file.read()
+        except FileNotFoundError:
+            print("file not found exiting")
+            sys.exit(0)
+        try:
+            #sends file information to server
+            fs.sendmsg(inputFile.strip().encode('utf-8') + b"\'start\'")
+        except BrokenPipeError:
+            print("disconnected start again")
+            sys.exit(0)
+        #send 100 bits at a time
+        while len(data) >= 100:
+            line = data[:100]
+            data = data[100:]
+            try:
+                fs.sendmsg(line)
+                print("received:", fs.receivemsg())
+            except BrokenPipeError:
+                print("disconnected start again")
+                sys.exit(0)
+        #sends left over bits
+        if len(data) > 0:
+            fs.sendmsg(data)
+            print("received:", fs.receivemsg())
+        try:
+        #tells server file has ended
+            fs.sendmsg(b"\'end\'")
+            print("received:", fs.receivemsg())
+        except BrokenPipeError:
+            print("disconnected start again")
+            sys.exit(0)
 
 
-       print("sending hello world")
-       fs.sendmsg(b"hello world")
-       print("received:", fs.receivemsg())
-
-       fs.sendmsg(b"hello world")
-       print("received:", fs.receivemsg())
-
-for i in range(100):
-    ClientThread(serverHost, serverPort, debug)
+ClientThread(serverHost, serverPort, debug)
 
--- networkThreads/serverThread.py
+++ networkThreads/serverThread.py
@@ -4,12 +4,12 @@ from threading import Thread
 from framedSock import FramedStreamSock
 if not  os.path.isdir("Server"):
     os.makedirs("Server")
-os.chdir("Server")
+    os.chdir("Server")
 switchesVarDefaults = (
     (('-l', '--listenPort') ,'listenPort', 50001),
-    (('-d', '--debug'), "debug", False), # boolean (set if present)
-    (('-?', '--usage'), "usage", False), # boolean (set if present)
-    )
+(('-d', '--debug'), "debug", False), # boolean (set if present)
+(('-?', '--usage'), "usage", False), # boolean (set if present)
+)
 
 progname = "ServerThread"
 paramMap = params.parseParams(switchesVarDefaults)
@@ -32,16 +32,61 @@ class ServerThread(Thread):
         self.fsock, self.debug = FramedStreamSock(sock, debug), debug
         self.start()
     def run(self):
+        if not "Server" in os.getcwd():
+            os.chdir("Server")
+        start = self.fsock.receivemsg()
+        try:
+            start = start.decode()
+        except AttributeError:
+            print("error exiting: ", start)
+            sys.exit(0)
+
+        count = 0
+        for char in start:
+                    if char.isalpha():
+                        break
+                    else:
+                        count = count + 1
+        start = start[count:]
+
+
+        #tells server where file name ends
+        start = start.split("\'start\'")
+
+        #opening file
+        file = open((start[0] ).strip(),"wb+")
+
+        #Receives input while file has not ended
         while True:
-            msg = self.fsock.receivemsg()
-            if not msg:
-                if self.debug: print(self.fsock, "server thread done")
-                return
-            requestNum = ServerThread.requestCount
-            time.sleep(0.001) #if file in dictionary stop here
-            ServerThread.requestCount = requestNum + 1
-            msg = ("%s! (%d)" % (msg, requestNum)).encode()
-            self.fsock.sendmsg(msg)
+            #error handling
+            try:
+                payload = self.fsock.receivemsg()
+            except:
+                pass
+
+            #checking for debugging
+            if debug: print("rec'd: ", payload)
+            if not payload:
+                break
+            #checking if end of file else writes to file
+            if b"\'end\'" in payload:
+                self.fsock.sendmsg(payload)
+                file.close()
+                break
+            else:
+                file.write(payload)
+                self.fsock.sendmsg(payload)
+
+        # while True:
+        #     msg = self.fsock.receivemsg()
+        #     if not msg:
+        #         if self.debug: print(self.fsock, "server thread done")
+        #         return
+        #     requestNum = ServerThread.requestCount
+        #     time.sleep(0.001) #if file in dictionary stop here
+        #     ServerThread.requestCount = requestNum + 1
+        #     msg = ("%s! (%d)" % (msg, requestNum)).encode()
+        #     self.fsock.sendmsg(msg)
 
 
 while True:
